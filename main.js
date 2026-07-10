993d9cce3a81df19a662b793a286ea30872b640d
fajarjunioredit-netizen <fajarjunioredit@gmail.com>
2026-07-10 23:56:36 +0700

    Refactor container display logic on page load
    Remove initial hiding of the container and update the onload function.

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// Sembunyikan bunga saat pertama kali
-document.querySelector(".container").style.display = "none";
-
-// Saat tombol diklik
-document.getElementById("startBtn").onclick = function () {
-    document.getElementById("intro").style.display = "none";
-    document.querySelector(".container").style.display = "block";
+onload = () =>{
+        document.body.classList.remove("container");
 };
