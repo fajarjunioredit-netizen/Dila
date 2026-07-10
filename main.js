e313893a8c1729cedf66c3c66e66b4cf2bd9c850
fajarjunioredit-netizen <fajarjunioredit@gmail.com>
2026-07-10 23:45:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-onload = () =>{
-        document.body.classList.remove("container");
+// Sembunyikan bunga saat pertama kali
+document.querySelector(".container").style.display = "none";
+
+// Saat tombol diklik
+document.getElementById("startBtn").onclick = function () {
+    document.getElementById("intro").style.display = "none";
+    document.querySelector(".container").style.display = "block";
 };
